893961d6a631d34e190cae1590bf8a155e213a56
abhimet <abhimetuku@gmail.com>
2026-02-26 18:02:27 -0500

    changed return statement as hw was asking for root, which would be at index [0]

--- HW2.py
+++ HW2.py
@@ -51,7 +51,7 @@ class HomeWork2:
     # you can see the examples in p2_traversals.csv
 
     def prefixNotationPrint(self, head: TreeNode) -> list:
-        pass
+        
 
     # Problem 2.2: Use in-order traversal (left, root, right) for infix notation with appropriate parentheses.
     # return an array of elements of an infix expression
